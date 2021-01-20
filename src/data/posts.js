c6bddaf8c33f81a52c5d24cd634f408aa78ba847
yhchan0918 <yhchan0918@gmail.com>
2021-01-20 22:20:36 +0800

    add dummy data

---
+++ src/data/posts.js
@@ -0,0 +1,58 @@
+export default [
+  {
+    id: 'p1',
+    videoUri:
+      'https://tiktokbucket.s3-ap-southeast-1.amazonaws.com/pexels-anastasia-shuraeva-6013206.mp4',
+    user: {
+      id: 'u1',
+      username: '@yhchan0918',
+      imageUri:
+        'https://media-exp1.licdn.com/dms/image/C5603AQGny5SpwxHumw/profile-displayphoto-shrink_800_800/0/1599634941866?e=1616630400&v=beta&t=5kQxrr6H-TuQa_AyK7wIuHsoSIvk-DzsckPSfREJvRc',
+    },
+    description: 'Love it',
+    songName: 'YT - Broken Heart',
+    songImage:
+      'https://media-exp1.licdn.com/dms/image/C5603AQGny5SpwxHumw/profile-displayphoto-shrink_800_800/0/1599634941866?e=1616630400&v=beta&t=5kQxrr6H-TuQa_AyK7wIuHsoSIvk-DzsckPSfREJvRc',
+    likes: 123,
+    comments: 12,
+    shares: 44,
+  },
+
+  {
+    id: 'p2',
+    videoUri:
+      'https://tiktokbucket.s3-ap-southeast-1.amazonaws.com/production+ID_4057150.mp4',
+    user: {
+      id: 'u1',
+      username: 'KIDIIN',
+      imageUri:
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ8rol7JqhyiG7KG49RQOrv0VacN4sfYWaWRQ&usqp=CAU',
+    },
+    description: 'hahahah oh boy @borat',
+    songName: 'NF - The Beat',
+    songImage:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ8rol7JqhyiG7KG49RQOrv0VacN4sfYWaWRQ&usqp=CAU',
+    likes: 123,
+    comments: 12,
+    shares: 44,
+  },
+
+  {
+    id: 'p3',
+    videoUri:
+      'https://d8vywknz0hvjw.cloudfront.net/fitenium-media-prod/videos/45fee890-a74f-11ea-8725-311975ea9616/proccessed_720.mp4',
+    user: {
+      id: 'u1',
+      username: 'User 3',
+      imageUri:
+        'https://influencermarketinghub.com/wiki/wp-content/uploads/2020/08/1657369265111046_c5_720x720.jpeg',
+    },
+    description: 'hahahah oh boy @borat',
+    songName: 'IU - Eight',
+    songImage:
+      'https://influencermarketinghub.com/wiki/wp-content/uploads/2020/08/1657369265111046_c5_720x720.jpeg',
+    likes: 123,
+    comments: 12,
+    shares: 44,
+  },
+];
